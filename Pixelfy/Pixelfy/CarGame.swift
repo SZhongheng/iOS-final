1c522b096888bca69651a20d2adcfaa37ac41a78
SZhongheng <35933371+SZhongheng@users.noreply.github.com>
2019-05-05 01:05:04 -0400

    Add files via upload

---
+++ Pixelfy/Pixelfy/CarGame.swift
@@ -0,0 +1,12 @@
+//
+//  CarGame.swift
+//  Pixelfy
+//
+//  Created by Mac on 5/2/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+import GameplayKit
--- Pixelfy/Pixelfy/Pixelfy/GameViewController.swift
+++ Pixelfy/Pixelfy/Pixelfy/GameViewController.swift
@@ -11,19 +11,13 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-    
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
-    
         if let view = self.view as! SKView? {
-            
-            if let scene = SKScene(fileNamed: "StartScene") {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -34,17 +28,14 @@ class GameViewController: UIViewController {
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsNodeCount = false
         }
-        
-        
-        
     }
-    
+
     override var shouldAutorotate: Bool {
         return true
     }
-    
+
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -52,29 +43,8 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-    
+
     override var prefersStatusBarHidden: Bool {
         return true
     }
-    
-    
-    
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
 }
-
-
